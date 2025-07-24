9d8c22294ded684c50d0fc83ac5428fbcc296983
Will D <diweizhi@gmail.com>
2025-07-24 14:45:31 +1000

    fetch comments and comment by reacthook

--- pages/posts/[postId]/comments/[commentId]/index.tsx
+++ pages/posts/[postId]/comments/[commentId]/index.tsx
@@ -1,15 +1,48 @@
+import { getCurrentTime } from '@/lib';
 import { useRouter } from 'next/router';
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+
+interface Comment {
+  id: number;
+  body: string;
+}
 
 export default function Page() {
   const router = useRouter();
   const id = router.query.commentId;
-  if (!id) {
-    return;
-  }
+  const [dt, setDt] = useState('');
+  const [data, setData] = useState<Comment>();
+
+  const fetchComment = () => {
+    if (!id) {
+      return;
+    }
+    fetch(`https://dummyjson.com/comments/${id}`)
+      .then((response) => {
+        return response.json();
+      })
+      .then((reply) => {
+        console.log(reply);
+        setDt(getCurrentTime());
+        setData(reply);
+      });
+  };
+
+  useEffect(() => {
+    fetchComment();
+  }, [id]);
+
   return (
     <main>
       <h1>Comment {id}</h1>
+      {data ? (
+        <>
+          <h4>{dt}</h4>
+          <p>{data.body}</p>
+        </>
+      ) : (
+        <p>Loading...</p>
+      )}
     </main>
   );
 }
--- pages/posts/[postId]/comments/index.tsx
+++ pages/posts/[postId]/comments/index.tsx
@@ -1,9 +1,39 @@
-import React from 'react';
+import { getCurrentTime } from '@/lib';
+import React, { useEffect, useState } from 'react';
+
+interface Comment {
+  id: number;
+  body: string;
+}
 
 export default function Page() {
+  const [dt, setDt] = useState('');
+  const [data, setData] = useState<Comment[]>([]);
+
+  const fetchComments = () => {
+    fetch('https://dummyjson.com/comments')
+      .then((response) => {
+        return response.json();
+      })
+      .then((reply) => {
+        console.log(reply);
+        setData(reply.comments);
+        setDt(getCurrentTime());
+      });
+  };
+
+  useEffect(() => {
+    fetchComments();
+  }, []);
+
   return (
     <main>
       <h1>comments</h1>
+      <h4>{dt}</h4>
+      <ul>
+        {data &&
+          data.map((comment) => <li key={comment.id}>{comment.body}</li>)}
+      </ul>
     </main>
   );
 }
