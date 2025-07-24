f05ac49279a7d82b3b47d347c140f5fc4ca3543f
Will D <diweizhi@gmail.com>
2025-07-24 15:22:49 +1000

    get static props for post

--- pages/posts/[postId]/index.tsx
+++ pages/posts/[postId]/index.tsx
@@ -1,6 +1,11 @@
 import { getCurrentTime } from '@/lib';
-import { useRouter } from 'next/router';
-import React, { useEffect, useState } from 'react';
+import { GetStaticPropsContext } from 'next';
+
+interface Props {
+  id: number;
+  dt: string;
+  post: Post;
+}
 
 interface Post {
   id: number;
@@ -8,40 +13,45 @@ interface Post {
   body: string;
 }
 
-export default function Page() {
-  const router = useRouter();
-  const id = router.query.postId;
-
-  const [dt, setDt] = useState('');
-  const [data, setData] = useState<Post | null>(null);
-
-  const fetchPost = () => {
-    fetch(`https://dummyjson.com/posts/${id}`)
-      .then((response) => response.json())
-      .then((reply) => {
-        console.log(reply);
-        setData(reply);
-        setDt(getCurrentTime());
-      });
-  };
-
-  useEffect(() => {
-    if (!id) return;
-    fetchPost();
-  }, [id]);
-
+export default function Page({ id, dt, post }: Props) {
   return (
     <main>
       <h1>Post {id}</h1>
-      {data ? (
+      {post ? (
         <>
           <h4>{dt}</h4>
-          <p>{data.title}</p>
-          <p>{data.body}</p>
+          <p>{post.title}</p>
+          <p>{post.body}</p>
         </>
       ) : (
         <p>Loading...</p>
       )}
     </main>
   );
+}
+
+export async function getStaticPaths() {
+  return {
+    paths: [{ params: { postId: '1' } }, { params: { postId: '2' } }],
+    fallback: false,
+  };
+}
+
+export async function getStaticProps(context: GetStaticPropsContext) {
+  if (!context.params || !context.params.postId) {
+    return {
+      notFound: true,
+    };
+  }
+  const id = context.params.postId;
+  const dt = getCurrentTime();
+  const response = await fetch(`https://dummyjson.com/posts/${id}`);
+  const reply = await response.json();
+  return {
+    props: {
+      id,
+      dt,
+      post: reply,
+    },
+  };
 }
