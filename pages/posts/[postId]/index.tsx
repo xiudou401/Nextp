410c8f5d0154844f48ea597c6dc7ed35214bf888
Will D <diweizhi@gmail.com>
2025-07-24 13:54:02 +1000

    fetch post by reacthook

--- pages/posts/[postId]/index.tsx
+++ pages/posts/[postId]/index.tsx
@@ -1,15 +1,47 @@
+import { getCurrentTime } from '@/lib';
 import { useRouter } from 'next/router';
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+
+interface Post {
+  id: number;
+  title: string;
+  body: string;
+}
 
 export default function Page() {
   const router = useRouter();
   const id = router.query.postId;
 
-  if (!id) return null;
+  const [dt, setDt] = useState('');
+  const [data, setData] = useState<Post | null>(null);
+
+  const fetchPost = () => {
+    fetch(`https://dummyjson.com/posts/${id}`)
+      .then((response) => response.json())
+      .then((reply) => {
+        console.log(reply);
+        setData(reply);
+        setDt(getCurrentTime());
+      });
+  };
+
+  useEffect(() => {
+    if (!id) return;
+    fetchPost();
+  }, [id]);
 
   return (
     <main>
       <h1>Post {id}</h1>
+      {data ? (
+        <>
+          <h4>{dt}</h4>
+          <p>{data.title}</p>
+          <p>{data.body}</p>
+        </>
+      ) : (
+        <p>Loading...</p>
+      )}
     </main>
   );
 }
