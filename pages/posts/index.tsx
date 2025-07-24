462d611e0517b17a36cadaa20de4af5f1d7121a5
Will D <diweizhi@gmail.com>
2025-07-24 13:18:09 +1000

    fetch posts by reacthook

--- pages/posts/index.tsx
+++ pages/posts/index.tsx
@@ -1,9 +1,42 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { getCurrentTime } from '../../lib';
+
+interface Post {
+  id: number;
+  title: string;
+}
 
 export default function Page() {
+  const [dt, setDt] = useState('');
+  const [data, setData] = useState<Post[]>([]);
+
+  const fetchData = () => {
+    fetch('https://dummyjson.com/posts')
+      .then((response) => {
+        return response.json();
+      })
+      .then((reply) => {
+        console.log(reply);
+        setData(reply.posts);
+        setDt(getCurrentTime());
+      });
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
   return (
     <main>
       <h1>posts</h1>
+      <h4>{dt}</h4>
+      <ul>
+        {data &&
+          data.map((item) => (
+            <li key={item.id}>
+              <h4>{item.title}</h4>
+            </li>
+          ))}
+      </ul>
     </main>
   );
 }
