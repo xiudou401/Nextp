27a14850a4c8a4dc308103f0e54e6520dc1c5447
Will D <diweizhi@gmail.com>
2025-07-25 15:36:40 +1000

    get static props for posts

--- pages/posts/index.tsx
+++ pages/posts/index.tsx
@@ -1,11 +1,37 @@
+import { getCurrentTime } from '@/lib';
 import React from 'react';
 
-export default function Page() {
+interface Props {
+  dt: string;
+  posts: Post[];
+}
+
+interface Post {
+  id: number;
+  title: string;
+}
+
+export default function Page({ dt, posts }: Props) {
   return (
     <main>
       <h1>Posts</h1>
+      {posts.map((post) => (
+        <ul key={post.id}>
+          <li>{post.title}</li>
+        </ul>
+      ))}
     </main>
   );
 }
 
-export async function getStaticProps() {}
+export async function getStaticProps() {
+  const dt = getCurrentTime();
+  const response = await fetch('https://dummyjson.com/posts');
+  const reply = await response.json();
+  return {
+    props: {
+      dt,
+      posts: reply.posts,
+    },
+  };
+}
